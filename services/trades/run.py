f6221c304b0907acfeab16989f7f4d4a403f0134
mvoss02 <m_l_voss@web.de>
2024-12-10 16:52:50 +0100

    Push mock data to Kafka Topic
    Next I will use the Kraken API to push real data.

--- services/trades/run.py
+++ services/trades/run.py
@@ -1,8 +1,60 @@
 from loguru import logger
+from quixstreams import Application
 
-def main():
+from kraken_api.mock_data import KrakenMockAPI
+
+def main(kafka_broker_address: str, kafka_topic: str):
+    """
+    Does 2 things:
+    1. Reads trades from Kraken Websocket API
+    2. Pushes trades to a Kraken topic
+    
+    Args:
+        kafka_broker_address: str
+        kafka_topic: str
+        
+    Returns: 
+        None
+    """
     logger.info('Start the trades service')
+    
+    # Get the mock data
+    kraken_api = KrakenMockAPI(pair='BTC/USD')
+    
+    # Init the Quix Streams app.
+    # This class handles all the low-level details to connect to Kafka.
+    app = Application(
+        broker_address=kafka_broker_address,
+    )
+    
+    # Define a topic with JSON serialization
+    topic = app.topic(name=kafka_topic, value_serializer='json')
+    
+    with app.get_producer() as producer:
+        while True:
+            trades = kraken_api.get_trades()
+            
+            for trade in trades:
+                # Seralize the trade as bytes
+                message = topic.serialize(
+                    key=trade.pair,
+                    value=trade.to_str(),
+                )
+                
+                # Push the seralized message to the topic
+                producer.produce(
+                    topic=topic.name, value=message.value, key=message.key
+                )
+                
+                logger.info(f'Pushed trade to Kafka: {trade}')
+        
 
 
 if __name__ == "__main__":
-    main()
+    from config import config
+    
+    main(
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_topic=config.kafka_topic,
+        
+    )
