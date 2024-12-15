90471dffd3687e82328e7770b38ffdca7e1482a6
mvoss02 <m_l_voss@web.de>
2024-12-15 11:57:52 +0100

    Added the possibility to ingest historical data
    We can now not only connect to the websocket Kraken API, but also to the
    rest API which allows us to collect historical trades.

--- services/trades/run.py
+++ services/trades/run.py
@@ -1,6 +1,6 @@
-from typing import Union
-
-from kraken_api.mock_data import KrakenMockAPI
+from kraken_api.base import TradesAPI
+from kraken_api.mock import KrakenMockAPI
+from kraken_api.rest import KrakenRestAPI
 from kraken_api.websocket import KrakenWebsocketAPI
 from loguru import logger
 from quixstreams import Application
@@ -9,44 +9,44 @@ from quixstreams import Application
 def main(
     kafka_broker_address: str,
     kafka_topic: str,
-    kraken_api: Union[KrakenMockAPI, KrakenWebsocketAPI],
+    trades_api: TradesAPI,
 ):
     """
-    Does 2 things:
-    1. Reads trades from Kraken Websocket API
-    2. Pushes trades to a Kraken topic
+    It does 2 things:
+    1. Reads trades from the Kraken API and
+    2. Pushes them to a Kafka topic.
 
     Args:
         kafka_broker_address: str
         kafka_topic: str
-        kraken_api: Union[KrakenMockAPI, KrakenWebsocketAPI]
+        trades_api: TradesAPI with 2 methods: get_trades and is_done
 
     Returns:
         None
     """
     logger.info('Start the trades service')
 
-    # Init the Quix Streams app.
+    # Initialize the Quix Streams application.
     # This class handles all the low-level details to connect to Kafka.
     app = Application(
         broker_address=kafka_broker_address,
     )
 
-    # Define a topic with JSON serialization
+    # Define the topic where we will push the trades to
     topic = app.topic(name=kafka_topic, value_serializer='json')
 
     with app.get_producer() as producer:
-        while True:
-            trades = kraken_api.get_trades()
+        while not trades_api.is_done():
+            trades = trades_api.get_trades()
 
             for trade in trades:
-                # Seralize the trade as bytes
+                # serialize the trade as bytes
                 message = topic.serialize(
                     key=trade.pair.replace('/', '-'),
                     value=trade.to_dict(),
                 )
 
-                # Push the seralized message to the topic
+                # push the serialized message to the topic
                 producer.produce(topic=topic.name, value=message.value, key=message.key)
 
                 logger.info(f'Pushed trade to Kafka: {trade}')
@@ -55,14 +55,26 @@ def main(
 if __name__ == '__main__':
     from config import config
 
-    # Get the mock data
-    # kraken_api = KrakenMockAPI(pair='BTC/USD')
+    # Initialize the Kraken API depending on the data source
+    if config.data_source == 'live':
+        kraken_api = KrakenWebsocketAPI(pairs=config.pairs)
+    elif config.data_source == 'historical':
+        kraken_api = KrakenRestAPI(pairs=config.pairs, last_n_days=config.last_n_days)
 
-    # Init the Kraken API and get real data from Kraken Websocket API
-    kraken_api = KrakenWebsocketAPI(pairs=config.pairs)
+        # # TODO: remove this once we are done debugging the KrakenRestAPISinglePair
+        # from kraken_api.rest import KrakenRestAPISinglePair
+        # kraken_api = KrakenRestAPISinglePair(
+        #     pair=config.pairs[0],
+        #     last_n_days=config.last_n_days,
+        # )
+
+    elif config.data_source == 'test':
+        kraken_api = KrakenMockAPI(pairs=config.pairs)
+    else:
+        raise ValueError(f'Invalid data source: {config.data_source}')
 
     main(
         kafka_broker_address=config.kafka_broker_address,
         kafka_topic=config.kafka_topic,
-        kraken_api=kraken_api,
+        trades_api=kraken_api,
     )
