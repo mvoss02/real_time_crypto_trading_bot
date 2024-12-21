79de1ca42b611abe24cd703e0a6b4286412b1fd5
mvoss02 <m_l_voss@web.de>
2024-12-21 17:12:39 +0100

    Added a scaffold for the price predictor service
    Still need to implement the logic. We nned to get the data from the
    features store and train an ML model.

---
+++ services/price-predictor/training.py
@@ -0,0 +1,16 @@
+from loguru import logger
+
+
+def train():
+    """
+    Does the following:
+    1. Reads feature data from the feature store
+
+
+    Everything implemented with CometML
+    """
+    logger.info('Hello from the ML model training job...')
+
+
+if __name__ == '__main__':
+    train()
